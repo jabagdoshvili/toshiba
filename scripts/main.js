967b5d3896100d2110de1486e53ba9c14022dca5
jabagdoshvili <jabagdoshvili@gmail.com>
2021-09-26 03:27:21 +0400

    ///

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,14 @@ var swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+AOS.init();
+
+
+$('.sm-menu-bar-icon').click(function() {
+  $('.mobile-menu').addClass('visible')
+})
+
+$('.mobile-menu > img').click(function() {
+  $('.mobile-menu').removeClass('visible')
+})
