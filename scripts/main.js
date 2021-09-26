6132dd028c4621714d1496868c8bc3bb70a5e333
jabagdoshvili <jabagdoshvili@gmail.com>
2021-09-26 11:34:42 +0400

    Mobile Issues

--- scripts/main.js
+++ scripts/main.js
@@ -19,3 +19,16 @@ $('.sm-menu-bar-icon').click(function() {
 $('.mobile-menu > img').click(function() {
   $('.mobile-menu').removeClass('visible')
 })
+
+$('.filter-tabs li').click(function() {
+  $(this).addClass('active').siblings().removeClass('active')
+  let attr = $(this).text().toLowerCase()
+
+  $(`.shop-list .shop-col`).hide()
+
+  if(attr == 'all') {
+    $(`.shop-list .shop-col`).show()
+  } else {
+    $(`.shop-list .shop-col[data-attr="${attr}"]`).show()
+  }
+})
